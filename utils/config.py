263e50da8f31073b9685a5e0887ae750d712b0ce
Regulus-Z <84190001+Regulus-Z@users.noreply.github.com>
2022-04-03 18:18:29 +0200

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,7 @@
 sample_rate = 16000
 cycle_len = 240000 # 2.5 s * 4000 4 s * 4000
 cycle_len_cough_heavy=80000# 5s
+cycle_len_va=80000# 5s
 win_length = 2048#256
 hop_length = 512#128
 mel_bins = 128
